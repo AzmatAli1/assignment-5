db283b615d0a159f5d8904ad5441acff2682ec56
AzmatAli1 <mdhafizazmat@gmail.com>
2022-02-16 23:00:46 +0600

    2nd commit

---
+++ css/js/calculation.js
@@ -0,0 +1,7 @@
+document.getElementById('income-button').addEventListener('click', function(){
+    const incomeInput = document.getElementById('income-button');
+    
+
+    const newIncomeInput = incomeInput.value;
+    const newIncomeInput = parseFloat(newIncomeInput);
+})
