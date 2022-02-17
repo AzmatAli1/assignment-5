be3aa076c3e4f3b398693909a43f49d7071fa0ac
AzmatAli1 <mdhafizazmat@gmail.com>
2022-02-17 23:26:23 +0600

    calculation all

--- js/calculation.js
+++ js/calculation.js
@@ -1,172 +1,54 @@
-function inputId(input) {
-    const inputValue = document.getElementById(input).value;
-    const totalInput = parseFloat(inputValue);
-    // if (totalInput < 0) {
-
-    //     document.getElementById('invalid-input').style.display = 'block';
-    //     return;
-    // }
-    return totalInput;
+function input(input) {
+    const incomeInput = document.getElementById(input).value;
+    const newInput = parseFloat(incomeInput);
+    return newInput;
 }
-
-
-// Evant handler calculate
-
-
-document.getElementById('calculate-button').addEventListener('click', function () {
-
-    const income = inputId('income');
-    const foodInput = inputId('food-input');
-    const rentInput = inputId('rent-input');
-    const clothesInput = inputId('clothes-input')
-
-
-    // Error Expenses
-
-
-    const expenses = document.getElementById('total-input');
-    expenses.innerText = (foodInput + rentInput + clothesInput);
+// Evant handler for Calculate button
+document.getElementById('expenses').addEventListener('click', function () {
+    const income = input('income');
+    const foodCost = input('food-cost');
+    const rentCost = input('rent-cost');
+    const clochesCost = input('clothes-cost')
+    //my expenses
+    const expenses = document.getElementById('more-expenses');
+    expenses.innerText = (foodCost + rentCost + clochesCost);
     const totalExpenses = expenses.innerText;
-    if ((foodInput < 0 || rentInput < 0 || clothesInput < 0)) {
-        document.getElementById('invalid-input').style.display = 'block';
-        expenses.innerText = '000';
+    if ((foodCost < 0 || rentCost < 0 || clochesCost < 0)) {
+        document.getElementById('input-text').style.display = 'block';
+        expenses.innerText = '00';
         balance.innerText = income - totalExpenses;
     }
-    else if (totalExpenses<income ) {
+    else if (income > totalExpenses) {
         const balance = document.getElementById('balance');
         balance.innerText = income - totalExpenses;
-        document.getElementById('invalid-message').style.display = 'none';
-        document.getElementById('invalid-input').style.display = 'none';
+        document.getElementById('input-title').style.display = 'none';
+        document.getElementById('input-text').style.display = 'none';
     }
     else {
-        document.getElementById('invalid-message').style.display = 'block';
-        balance.innerText = '000';
+        document.getElementById('input-title').style.display = 'block';
+        balance.innerText = '00';
     }
 });
-
-
-/* // Eevnt handler for save button
-
-
-document.getElementById('save').addEventListener('click', function () {
-    const savingInput = inputId('saving-input')
+// handler save button
+document.getElementById('save-incom').addEventListener('click', function () {
+    const savingInput = input('saving-input')
     const balanceText = document.getElementById('balance').innerText;
     const balance = parseFloat(balanceText);
-
-
-    //event savings
-
-
+    //Amount save
     const savingAmount = document.getElementById('saving-amout');
     savingAmount.innerText = (balance / 100 * savingInput);
     const saveAmount = savingAmount.innerText;
     if (savingInput < 0 || savingInput > 100) {
         document.getElementById('wrong-save').style.display = 'block';
         const remaining = document.getElementById('remaining-balance');
-        remaining.innerText = '000';
-        savingAmount.innerText = '000';
+        remaining.innerText = '00';
+        savingAmount.innerText = '00';
     }
-        //Remaining Balance
-
-        else {
-
+    else {
+        //remaining
         const remainingBalance = balance - saveAmount;
         const remaining = document.getElementById('remaining-balance');
         remaining.innerText = remainingBalance;
         document.getElementById('wrong-save').style.display = 'none';
     }
 });
-
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//handle calculate-button
-/* 
-document.getElementById('calculate-button').addEventListener('click', function(){
-    const foodCost = document.getElementById('food-input');
-    const totalFoodAmount = foodCost.value;
-    const totalFoodCost = parseFloat(totalFoodAmount);
-    
-
-
-    const rentCost = document.getElementById('rent-input');
-    const totalRentAmount = rentCost.value;
-    const totalRentCost = parseFloat(totalRentAmount); 
-    
-
-    const clothesCost = document.getElementById('clothes-input');
-    const totalClthesAmount = clothesCost.value;
-    const totalClothesCost = parseFloat(totalClthesAmount);
-
-
-    const calculateTotal = document.getElementById('expenses-input');
-    calculateTotal.innerText = parseFloat(totalFoodCost + totalRentCost + totalClothesCost);
-    const cost = calculateTotal.innerText;
-
-// handle income and balance
-    const income = document.getElementById('income-input');
-    const totalIncomeAmount = income.value;
-    const totalIncome = parseFloat(totalIncomeAmount);
-
-    const incomeTotal = totalIncome-cost;
-    const balanceTotal = document.getElementById('balance-input');
-    balanceTotal.innerText = incomeTotal;
-    
-  
-});
-
-// handle savings percent
-
-
-document.getElementById('save-percent').addEventListener('click', function(){
-    const saveInput = document.getElementById('save-input');
-    const totalSaveAmount = saveInput.value;
-    const totalSave = parseFloat(totalSaveAmount); 
-    const total = document.getElementById('save-input').innerText;
-    const totalBalance = parseFloat(total); 
-    console.log('ok');
-
-
-
-    const savingInput = document.getElementById('savings-amount');
-    const saving = parseFloat(totalBalance/ 100 * savingInput);
-    savingInput.innerText = saving;
-    const saveAmount = savingInput.innerText;
-
-    const remainingBalance = totalBalance - saveAmount;
-    const remaining = document.getElementById('remainingBalance');
-   console.log('ok code');
-});
-    
- */
-
-
